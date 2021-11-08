da64e41c1677f5a1b9b209c370b1f28c4af5cf9e
mrbin2903 <hhhien2903@gmail.com>
2021-11-08 14:11:25 +0700

    1st commit

---
+++ src/test/java/com/example/crud_redis_hhh/CrudRedisHhhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_redis_hhh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRedisHhhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
